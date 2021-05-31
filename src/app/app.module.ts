58d65c0ba1c6dcd4f3d2d652e2e0aa620041fb3f
Siddharth <sidhu.pabbis@gmail.com>
2021-05-31 19:14:41 +0530

    Nav-Bar and Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DietfixHomeComponent } from './dietfix-home/dietfix-home.component';
+import { DietfixRecipesComponent } from './dietfix-recipes/dietfix-recipes.component';
+import { DietfixContactusComponent } from './dietfix-contactus/dietfix-contactus.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DietfixHomeComponent,
+    DietfixRecipesComponent,
+    DietfixContactusComponent,
+   
   ],
   imports: [
     BrowserModule,
